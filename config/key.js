ec95d93ed9d28d0692b20661eae1804ad8a3df8a
khititach <khititach58@email.nu.ac.th>
2019-10-28 15:38:48 -0700

    change key mongodb cloud

--- config/key.js
+++ config/key.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // MongoUrl: 'mongodb+srv://admin:admin@epayment-arvvq.gcp.mongodb.net/Epayment?retryWrites=true&w=majority'
-    MongoUrl: 'mongodb://localhost:27010/epaymentDB'
+    MongoUrl: 'mongodb+srv://admin:admin@epayment-arvvq.gcp.mongodb.net/Epayment?retryWrites=true&w=majority'
 }
